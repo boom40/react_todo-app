70b718e3c56586036e1bf195ae7ed972ef056100
boom40 <kickgames675@gmail.com>
2019-12-15 22:45:39 +0200

    Updates

---
+++ precache-manifest.5454805cbb508adefcb3b9a202318883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acddc0e7f50ced5c29fd1434250cdb4",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "f66dcdfa97513bd706e6",
+    "url": "/react_todo-app/static/css/main.04ee3606.chunk.css"
+  },
+  {
+    "revision": "5a76bad6fc1e05f761a2",
+    "url": "/react_todo-app/static/js/2.583b85e5.chunk.js"
+  },
+  {
+    "revision": "f66dcdfa97513bd706e6",
+    "url": "/react_todo-app/static/js/main.63becdba.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
